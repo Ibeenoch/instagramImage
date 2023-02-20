6480b9a7bed54851833e3af339bb2e0f42af34d5
Ibeenoch <94024099+Ibeenoch@users.noreply.github.com>
2023-02-20 09:50:07 +0100

    Update DropArea3.js

--- src/components/DropArea3.js
+++ src/components/DropArea3.js
@@ -37,8 +37,7 @@ console.log('is deleted', isDelete)
   const dateString = date.toISOString().substr(0, 10);
 
 
-  const url = 'http://localhost:3030';
-
+  const url = 'https://logistics-backend.onrender.com'
 
 
   const getplanner = async () => {
@@ -55,7 +54,6 @@ console.log('is deleted', isDelete)
  
        if(getPlanner){
         setItems(getPlanner); 
-        setMoved(true);
        }else{
         setItems([])
        }

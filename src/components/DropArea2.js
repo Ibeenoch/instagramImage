725eb5a3810a733f577f5033e04e3a501f1546c3
Ibeenoch <94024099+Ibeenoch@users.noreply.github.com>
2023-02-21 21:09:35 +0100

    Update DropArea2.js

--- src/components/DropArea2.js
+++ src/components/DropArea2.js
@@ -2,18 +2,21 @@ import axios from 'axios';
 import React, { useState, useEffect, Children } from 'react'
 
 function DropArea2({ getid, setMoved, customers }) {
-  const [items, setItems] = useState([]);
-  const [id, setId] = useState(0)
+  let getPlanner = JSON.parse(localStorage.getItem('planner1'));
+  const [items, setItems] = useState( getPlanner ? getPlanner : []);
+  const [id, setId] = useState(0) 
   const [isDrop, setIsDrop] = useState(false);
   const [isDelete, setIsDelete] = useState(false);
   
+
+ 
 
   const handleDelete = async () => {
     console.log(items)
     let id = items && items[0] && items[0].customer_id
 
     setIsDelete(true)
-    localStorage.removeItem('planner1')   
+    localStorage.removeItem('planner2')   
 
  return  await axios.delete(`${url}/planner/${id}`)
     .then((res) => {
@@ -24,7 +27,6 @@ function DropArea2({ getid, setMoved, customers }) {
     
   }
  
-console.log('is deleted', isDelete)
 
 
 
@@ -34,8 +36,7 @@ console.log('is deleted', isDelete)
   const dateString = date.toISOString().substr(0, 10);
 
 
-  const url = 'https://logistics-backend.onrender.com'
-
+  const url = 'https://logisticsslot-backend.ibeenoch.repl.co'
 
   const getplanner = async () => {
     // fetch items from the database
@@ -49,7 +50,7 @@ console.log('is deleted', isDelete)
         localStorage.setItem('planner2', JSON.stringify(data));
         let getPlanner = JSON.parse(localStorage.getItem('planner2'));
  
-       if(getPlanner){
+       if(data){
         setItems(getPlanner); 
         setMoved(true);
        }else{
@@ -60,16 +61,6 @@ console.log('is deleted', isDelete)
       .catch((error) => console.log(error));
   }
 
-
-  useEffect(() => {
-    let getPlanner = JSON.parse(localStorage.getItem('planner2'));
-    
-    if(getPlanner){
-      setItems(getPlanner); 
-     }else{
-      setItems([])
-     }
-  }, [getplanner])
 
         
   if(isDrop || isDelete ){

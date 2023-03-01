8ca83c0a7a8f765e903a2535c96d0bb99f352c7c
Ibeenoch <94024099+Ibeenoch@users.noreply.github.com>
2023-03-01 07:07:50 +0100

    Update Info.js
    J

--- src/components/Info.js
+++ src/components/Info.js
@@ -25,15 +25,21 @@ The web page displayed in your new window browser is important to us <br />
 here because authorization code that has been appended to the redirect URL. <br />
 If you look in your new window browser’s URL, you should see a URL that looks something like this: <br />
 
-<pre> https://localhost:3000?code=AQBvJwCZtYdj1zLH_5myoAA1GRRpDhs1vcHFMzB4gvRk6dLkq5dNd24EVZ5FD9WoqQhfSuo6arUB1<br />
-7MPu2gRqEzP6EpsAl-9_2eC9-L6mWYQdWDyarkwDSNEs8T3gvoH-WLMHzhwwd6DJqP5PxJGf2ve53m7aGMEua3MzV8FZQVz5AfwWPN3G87n25j<br />
-MBGgGGVj6G4pxJ9HqzNKmdpYK8GHKnRn_G03scHtUraFlEX5faCvz6ZO7Xw#_</pre>
+<pre> https://localhost:3000?code=AQBvJwCZtYdj1z<br />
+LH_5myoAA1GRRpDhs1vcHFMzB4gvRk6dLkq5dNd24EVZ5FD<br />
+9WoqQhfSuo6arUB17MPu2gRqEzP6EpsAl-9_2eC9-L6mWYQ<br />
+dWDyarkwDSNEs8T3gvoH-WLMHzhwwd6DJqP5PxJGf2ve53m<br />
+7aGMEua3MzV8FZQVz5AfwWPN3G87n25jBGgGGVj6G4pxJ9H<br />
+zNKmdpYK8GHKnRn_G03scHtUraFlEX5faCvz6ZO7Xw#_</pre>
 
 copy everything after <pre> <strong>code=</strong></pre>  up to (but not including) the ,<pre><strong>#_ </strong></pre> at the end. So, in the above URL, the authorization code would be something like this:
 
-<pre>AQBvJwCZtYdj1zLH_5myoAA1GRRpDhs1vcHFMzB4gvRk6dLkq5dNd24EVZ5FD9WoqQhfSuo6arUB17MPu2gRqEzP6EpsAl-9_2eC9-L6m<br />
-WYQdWDyarkwDSNEs8T3gvoH-WLMHzhwwd6DJqP5PxJGf2ve53m7aGMEua3MzV8FZQVz5AfwWPN3G87n25jMBGgGGVj6G4pxJ9HqzNKmdpYK8GH<br />
-KnRn_G03scHtUraFlEX5faCvz6ZO7Xw</pre>
+<pre>AQBvJwCZtYdj1zLH_5myoAA1GRRpDhs1vcHFMzB4gv<br />
+Rk6dLkq5dNd24EVZ5FD9WoqQhfSuo6arUB17MPu2gRqEzP<br />
+6EpsAl-9_2eC9-L6mkYQdWDyarkwDSNEs8T3gvoH-WLMHz<br />
+hwwd6DJqP5PxJGf2ve53m7aGMEua3MzV8FZQVz5AfwWPN3<br />
+G87n25jMBGgGGVj6G4pxJ9HqzNKmdpYK8GHknRn_G03scH<br />
+tUraFlEX5faCvz6ZO7Xw</pre>
 
 after copying the token, paste it into the input field and then hit the "Get Photos" button and it will display all your Instagram Photos
      </p>
